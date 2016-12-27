98896c8881cbf9576db0091ec75dda60b6642dd1
hdeadman <hal.deadman@gmail.com>
2016-12-27 10:00:52 -0500

    support systemd service style
    fix: handle versions like 1.11
    note: installs tmux instead of screen for systemd

---
+++ manifests/service_systemd.pp
@@ -0,0 +1,31 @@
+class minecraft::service_systemd {
+
+  file { 'minecraft service unit':
+    ensure  => present,
+    path    => '/etc/systemd/system/minecraft.service',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0744',
+    content => template($minecraft::systemd_template),
+  }
+
+  exec { "refresh minecraft service":
+    command     => 'systemctl daemon-reload',
+    refreshonly => true,
+    subscribe   => File['minecraft service unit'],
+    path        => ['/bin', '/usr/bin'],
+    notify      => Service['minecraft']
+  }
+
+  service { 'minecraft':
+    ensure    => running,
+    enable    => $minecraft::autostart,
+  }
+  
+  # remove non-systemd init script
+  file { 'minecraft_init':
+    ensure  => absent,
+    path    => '/etc/init.d/minecraft',
+  }
+
+}
